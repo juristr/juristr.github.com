73152e5ab4c54a4dcdeec14657cfd85974867274
Juri <juri.strumpflohner@gmail.com>
2013-01-10 08:15:13 +0100

    fixed g+ link

--- _plugins/imagepreview.rb
+++ _plugins/imagepreview.rb
@@ -1,20 +1,20 @@
-require 'nokogiri'
+# require 'nokogiri'
 
-module Jekyll
+# module Jekyll
 
-  module ImagePreview
+#   module ImagePreview
 
-    def imgprev(content)
-      doc = Nokogiri::HTML.fragment(content)
-      #doc.css('img').each do |img|
-      #  return img
-      #end
-      #return doc # | strip_html | truncatewords:75
-      #return content | strip_html | truncatewords:75;
-    end
+#     def imgprev(content)
+#       doc = Nokogiri::HTML.fragment(content)
+#       #doc.css('img').each do |img|
+#       #  return img
+#       #end
+#       #return doc # | strip_html | truncatewords:75
+#       #return content | strip_html | truncatewords:75;
+#     end
 
-  end
+#   end
 
-end
+# end
 
 #Liquid::Template.register_filter(Jekyll::ImagePreview)
