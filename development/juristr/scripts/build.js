d71d3f5522a711735cf4174a39d0935788a0e530
Juri <juri.strumpflohner@gmail.com>
2013-06-26 15:29:16 +0200

    removing unnecessary resources

--- development/juristr/scripts/build.js
+++
@@ -1,6 +0,0 @@
-//steal/js juristr/scripts/compress.js
-
-load("steal/rhino/rhino.js");
-steal('steal/build','steal/build/scripts','steal/build/styles',function(){
-	steal.build('juristr/scripts/build.html',{to: './'});
-});
--- development/juristr/scripts/clean.js
+++
@@ -1,6 +0,0 @@
-//steal/js juristr/scripts/compress.js
-
-load("steal/rhino/rhino.js");
-steal('steal/clean',function(){
-	steal.clean('juristr/juristr.html',{indent_size: 1, indent_char: '\t'});
-});
