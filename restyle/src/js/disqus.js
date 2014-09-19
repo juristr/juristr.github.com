72b73f42d4ae331ecc19e66e632e19444ef1f404
Juri <juri.strumpflohner@gmail.com>
2014-09-19 21:57:26 +0200

    refactors js in separate file

---
+++ restyle/src/js/disqus.js
@@ -0,0 +1,26 @@
+var disqus_shortname = 'juristr';
+
+(function(){
+  // comment count
+  var s = document.createElement('script'); s.async = true;
+  s.type = 'text/javascript';
+  s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+})
+
+
+(function(exports, undefined){
+  "use strict";
+
+  exports.disqus = {
+    loadDisqus: function(){
+      /* * * DON'T EDIT BELOW THIS LINE * * */
+      (function() {
+          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+          dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    }
+  };
+
+})(window);
