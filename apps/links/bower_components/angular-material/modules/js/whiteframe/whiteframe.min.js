fb00927b1e85ce05b00c076e4e573ef41f1a7578
juristr <juri.strumpflohner@gmail.com>
2015-09-26 15:40:11 +0200

    add possibility to submit new links from phone

---
+++ apps/links/bower_components/angular-material/modules/js/whiteframe/whiteframe.min.js
@@ -0,0 +1,7 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.11.0-rc2-master-587cd22
+ */
+!function(n,e,i){"use strict";e.module("material.components.whiteframe",[])}(window,window.angular);
