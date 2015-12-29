e9979c40259f003991f4eba09ae875b4f508f6d8
juristr <juri.strumpflohner@gmail.com>
2015-12-29 16:56:05 +0100

    upgrade to latest angular 2 beta version

--- apps/ng2beta/dist/services/ng2-progress.service.js
+++ apps/ng2beta/dist/services/ng2-progress.service.js
@@ -1,11 +1,9 @@
 System.register(['angular2/angular2', 'angular2/http'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
-        switch (arguments.length) {
-            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-        }
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
@@ -33,15 +31,15 @@ System.register(['angular2/angular2', 'angular2/http'], function(exports_1) {
                         var progressData = _this.data.milestonedata;
                         // let daysRemaining = this.calculateDaysRemaining(progressData);
                         // this.projectedDate = moment().add(daysRemaining, 'days');
-                        var startIdx = _this.data.milestonedata.length - _this.evaluationRange;
-                        var newOpenSum = 0;
-                        var newClosedSum = 0;
-                        for (var i = startIdx; i < _this.data.milestonedata.length; i++) {
-                            newOpenSum += _this.data.milestonedata[i].newOpen;
-                            newClosedSum += _this.data.milestonedata[i].newClosed;
-                        }
-                        _this.newOpenAvg = Math.round((newOpenSum / (_this.data.milestonedata.length - startIdx)));
-                        _this.newClosedAvg = Math.round((newClosedSum / (_this.data.milestonedata.length - startIdx)));
+                        // let startIdx = this.data.milestonedata.length - this.evaluationRange;
+                        // let newOpenSum: number = 0;
+                        // let newClosedSum: number = 0;
+                        // for (let i = startIdx; i < this.data.milestonedata.length; i++) {
+                        //   newOpenSum += this.data.milestonedata[i].newOpen;
+                        //   newClosedSum += this.data.milestonedata[i].newClosed;
+                        // }
+                        // this.newOpenAvg = Math.round((newOpenSum / (this.data.milestonedata.length - startIdx)));
+                        // this.newClosedAvg = Math.round((newClosedSum / (this.data.milestonedata.length - startIdx)));
                         // $('#releasedate').html(projectedDate.format('dddd, MMMM Do YYYY'));
                         // $('#currentPace').html(currentPace + '%');
                         // $('#resolvedIssuesPercent').html(progressData[progressData.length - 1].percent + '%');
