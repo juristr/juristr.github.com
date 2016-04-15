5725ae7a35498577e2832830e81c842355bc0bb8
juristr <juri.strumpflohner@gmail.com>
2016-04-15 11:04:07 +0200

    add twitter link

--- apps/ng2beta/_dev/src/ng2beta.ts
+++ apps/ng2beta/_dev/src/ng2beta.ts
@@ -48,7 +48,8 @@ import {LastUpdateCmp} from './components/last-update.component';
           Also, check out <a href="/blog/tags/#Angular.js">some of my articles on Angular 1.x</a> and <a href="/blog/tags/#Angular">Angular 2</a> and/or <a href="https://twitter.com/juristr">follow me on Twitter</a> for getting notified about new articles I'm releasing.<br/>
           I'm also working on a screencast series on Angular 2, so stay tuned ;)
         </p>-->
-        <br /><br />
+        <p style="font-size:19px">Remember to <strong>follow me <a href="https://twitter.com/juristr">on Twitter</a></strong> for getting updates around Angular 2!</p>
+        <br />
         <a href="https://github.com/angular/angular/milestones" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style="background-color: #ff9800; color: #fff">
           Verify milestone progress
         </a>
