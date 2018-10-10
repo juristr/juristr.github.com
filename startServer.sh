cbd55c13df32ca1212f08d86832cebc70ce5f444
Juri <juri.strumpflohner@r3-gis.com>
2018-10-10 14:09:03 +0200

    fix docker file setup

--- startServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker run \
-    -it \
-    --rm \
-    -v "$PWD":/usr/src/app:delegated \
-    -v site:/usr/src/app/_site \
-    -p "4000:4000" \
-    starefossen/github-pages
-    #juristr-blog
