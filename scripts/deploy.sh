57f3b9c41539f665614eed7907fb062072f0920e
Juri <juri.strumpflohner@gmail.com>
2019-01-06 16:52:00 +0100

    hard-publish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ mkdir public
 
 echo "creating master branch in public folder"
 # clone master branch from the local repo into a repo located within "public"
-git clone .git --branch master public
+# git clone .git --branch master public
 
 echo "generating hugo files"
 
